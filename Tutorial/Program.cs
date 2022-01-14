3cd8748f32d4aa6fd99695c04d661c142c2eece0
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-14 17:12:42 -0300

    .Linq namespace with arrays

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -1,24 +1,15 @@
-﻿namespace Tutorial
+﻿using System.Linq;
+
+namespace Tutorial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Sort a string
-            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
-            Array.Sort(cars);
-            foreach (string i in cars)
-            {
-                Console.WriteLine(i);
-            }
-
-            // Sort an int
             int[] myNumbers = { 5, 1, 8, 9 };
-            Array.Sort(myNumbers);
-            foreach (int i in myNumbers)
-            {
-                Console.WriteLine(i);
-            }
+            Console.WriteLine(myNumbers.Max());  // returns the largest value
+            Console.WriteLine(myNumbers.Min());  // returns the smallest value
+            Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
         }
     }
 }
