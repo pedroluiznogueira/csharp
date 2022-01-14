5d3ca79f0614966180b3bc42f9fa9b8971d02a1b
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-14 17:10:27 -0300

    arrays

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -4,15 +4,34 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 10; i = i + 2)
-            {
-                Console.WriteLine(i);
-            }
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 4)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);
+            //}
 
-            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
-            foreach (string i in cars)
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 4)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+
+            int i = 0;
+            while (i < 10)
             {
                 Console.WriteLine(i);
+                i++;
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine("showing");
             }
         }
     }
