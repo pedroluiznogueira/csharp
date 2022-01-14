a32c8133154fe1cc06bcc24950361c9b31786a5d
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-14 18:29:20 -0300

    classes and objects

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -4,17 +4,10 @@ namespace Tutorial
 {
     class Program
     {
-
-        static void MyMethod(string child1, string child2, string child3)
-        {
-            Console.WriteLine("The youngest child is: " + child3);
-        }
-
         static void Main(string[] args)
         {
-            MyMethod(child3: "John", child1: "Liam", child2: "Liam");
+            Car myCar = new Car();
+            Console.WriteLine(myCar.color);
         }
-
-        // The youngest child is: John
     }
 }
