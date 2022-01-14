ba8eaf8253fd99f61044bf5f4f8e152bee253005
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-14 17:00:11 -0300

    conditional and if statements

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -4,8 +4,24 @@
     {
         static void Main(string[] args)
         {
-            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            Console.WriteLine("The length of the txt string is: " + txt.Length);
+            int time = 22;
+            if (time < 10)
+            {
+                Console.WriteLine("Good morning.");
+            }
+            else if (time < 20)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+            // Outputs "Good evening."
+
+            int timeee = 20;
+            string result = (timeee < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
         }
     }
 }
