390ebd7a8d39717206a7538ee429546464b1ca11
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-14 19:42:31 -0300

    classes can implement multiple interfaces but only extend only one class, but it can extend a class and implement one or more interfaces at the same time, but the extended class must come before the interface(s)

--- Tutorial/model/Car.cs
+++ Tutorial/model/Car.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tutorial.interfaces;
 
 namespace Tutorial.model
 {
-    class Car : Vehicle
+    class Car : Vehicle, IBestVehicle
     {
+        public void greet()
+        {
+            Console.WriteLine("Hey I'm a best vehicle");
+        }
+
         public override void present()
         {
-            Console.WriteLine("I'm a car");
+            Console.WriteLine("Presenting myself");    
         }
     }
 }
