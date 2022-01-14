bdabf2664e8af59f1e412c82496d479ce2fbff87
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-14 16:43:39 -0300

    variables declarations, assignments and data types

---
+++ Tutorial/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring and assigning
+            int myNum = 5;
+
+            // declaring without assigning
+            string myText;
+            myText = "pedro";
+
+            char first = 'p', second = 'l'; 
+
+            // constant
+            const string noChange = "luiz";
+
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+
+            Console.WriteLine(myNum);
+            Console.WriteLine(myText);
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            Console.WriteLine(noChange);    
+        }
+    }
+}
